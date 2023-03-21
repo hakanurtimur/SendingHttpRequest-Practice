c53915b710e95bf8704e8424a0977fa41977b8f5
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-03-21 21:51:14 +0300

    Deleteing Dummy Datas and fetching data somewhere

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const fetchMovieHandler = () =>
+  {
+    fetch()
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList/>
       </section>
     </React.Fragment>
   );
